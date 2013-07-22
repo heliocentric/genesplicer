72f9580c71683d78d67bd736d2e397f830d83716
Dylan Cochran <heliocentric@gmail.com>
2013-07-22 06:54:11 -0400

    Add XML stuff to libsplicer so that we can save genomes.

--- libsplicer/src/com/github/heliocentric/genesplicer/Genome.java
+++ libsplicer/src/com/github/heliocentric/genesplicer/Genome.java
@@ -4,17 +4,45 @@
  */
 package com.github.heliocentric.genesplicer;
 
+import java.io.IOException;
+import nu.xom.*;
+
 /**
  *
  * @author helio
  */
 public class Genome {
-    public Chromosome[] Mother;
-    public Chromosome[] Father;
-    public Chromosome[] Mitochondrial;
-    public Genome() {
-        this.Mother = new Chromosome[12];
-        this.Father = new Chromosome[12];
-        this.Mitochondrial = new Chromosome[6];
-    }
+
+	public Chromosome[] Mother;
+	public Chromosome[] Father;
+	public Chromosome[] Mitochondrial;
+	public Document Root;
+	public String File;
+
+	public Genome() {
+	}
+
+	public void New() {
+	}
+
+	public int Open(String File) {
+		return 0;
+	}
+
+	public int Save() {
+		return this.SaveAs(this.File);
+	}
+
+	public int SaveAs(String File) {
+		this.File = File;
+		try {
+			Serializer serializer = new Serializer(System.out, "ISO-8859-1");
+			serializer.setIndent(4);
+			serializer.setMaxLength(64);
+			serializer.write(this.Root);
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+		return 0;
+	}
 }
