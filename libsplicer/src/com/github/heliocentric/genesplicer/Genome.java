5e4518dd05244368262fe67599bd5b69dc17c66b
Dylan Cochran <heliocentric@gmail.com>
2013-07-22 07:51:43 -0400

    Actually hook new,open,save,save as to Genome class.

--- libsplicer/src/com/github/heliocentric/genesplicer/Genome.java
+++ libsplicer/src/com/github/heliocentric/genesplicer/Genome.java
@@ -4,6 +4,8 @@
  */
 package com.github.heliocentric.genesplicer;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import nu.xom.*;
 
@@ -17,7 +19,7 @@ public class Genome {
 	public Chromosome[] Father;
 	public Chromosome[] Mitochondrial;
 	public Document Root;
-	public String File;
+	public File File;
 
 	public Genome() {
 	}
@@ -25,18 +27,18 @@ public class Genome {
 	public void New() {
 	}
 
-	public int Open(String File) {
+	public int Open(File file) {
 		return 0;
 	}
-
 	public int Save() {
 		return this.SaveAs(this.File);
 	}
 
-	public int SaveAs(String File) {
+	public int SaveAs(File File) {
 		this.File = File;
+		
 		try {
-			Serializer serializer = new Serializer(System.out, "ISO-8859-1");
+			Serializer serializer = new Serializer(new FileOutputStream(this.File), "UTF-8");
 			serializer.setIndent(4);
 			serializer.setMaxLength(64);
 			serializer.write(this.Root);
